45574a65e3f8437000c7d75e0c04cee2f30520e8
benamara <fatahbenamara87@hotmail.fr>
2020-08-30 20:22:04 +0200

    AJOUT des entite des albums

--- src/DataFixtures/ArtistFixtures.php
+++ src/DataFixtures/ArtistFixtures.php
@@ -6,7 +6,7 @@ use App\Entity\Artist;
 class ArtistFixtures extends BaseFixture
 {
     protected function loadData(){
-        $this->createMany(20, function(){
+        $this->createMany(20,'artist' ,function(){
             return (new Artist())
 
                     ->setName($this->faker->name)
--- src/DataFixtures/BaseFixture.php
+++ src/DataFixtures/BaseFixture.php
@@ -18,6 +18,9 @@ abstract class BaseFixture extends Fixture
     /** @var Generator */
     protected $faker;
 
+    /** @var array */
+    private $references = [];
+
     abstract protected function loadData();
     public function load(objectManager $manager)
     {
@@ -27,35 +30,52 @@ abstract class BaseFixture extends Fixture
         $this->manager->flush();
     }
 
-/**
- * @param int $count
- * @param collable $factory
- */
-protected function createMany(int $count, callable $factory){
- 
-    for ($i=0; $i < $count; $i++) { 
-        $entity = $factory();
+    /**
+     * @param int $count
+     * @param string $groupName
+     * @param collable $factory
+     */
+    protected function createMany(int $count, string $groupName, callable $factory){
+    
+        for ($i=0; $i < $count; $i++) { 
+            $entity = $factory();
 
-        if($entity === null){
-            throw new \LogicException('L\'Entité doit être retournée');        
+            if($entity === null){
+                throw new \LogicException('L\'Entité doit être retournée');        
+            }
+
+            $this->manager->persist($entity);
+
+            $reference = sprintf('%s_%d', $groupName, $i);
+            $this->addReference($reference, $entity);
+
         }
-
-        $this->manager->persist($entity);
+        
     }
 
 
+    /**
+     * @param string $groupName 
+     */
+    protected function getRandomReference(string $groupName)
+    {
+        if (!isset($this->references[$groupName])) {
+            $this->references[$groupName] = [];
 
+            foreach ($this->referenceRepository->getReferences() as $key => $ref) {
+                if (strpos($key, $groupName) === 0) {
+                    $this->references[$groupName][] = $key;
+                }
+            }
+        }
 
+        if ($this->references[$groupName] === []) {
+            throw new \Exception(sprintf('Aucune référence trouvée pour le groupe "%s"', $groupName));
+        }
 
-
-}
-
-
-
-
-
-
-
+        $randomReference = $this->faker->randomElement($this->references[$groupName]);
+        return $this->getReference($randomReference);
+    }
 
 
 }
